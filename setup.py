036c820372ce35be523a66e192ba5434d55763be
Igor <igor@amperka.ru>
2018-07-18 11:57:47 +0300

    add library TroykaCap

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name = 'troykaCap',
+    version = '1.0',
+    description = 'troykaCap',
+    author = 'acosinwork',
+    author_email = 'vasily@amperka.ru',
+    url = 'amperka.ru',
+    py_modules=['gpioexp'],
+)
