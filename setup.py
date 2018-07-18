7d74f2767d93ef61e697771f0a602439e87a1b4e
Igor <igor@amperka.ru>
2018-07-18 16:37:50 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,9 +1,9 @@
 from distutils.core import setup
 
 setup(
-    name = 'TroykaCap',
+    name = 'TroykaCapPython',
     version = '1.0',
-    description = 'TroykaCap',
+    description = 'TroykaCapPython',
     author = 'acosinwork',
     author_email = 'vasily@amperka.ru',
     url = 'amperka.ru',
